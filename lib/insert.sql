a9de813d6692a141900ec2b2aa4f3c34f1752241
Vaschell Mkenzie <mvaschell@gmail.com>
2023-08-02 15:38:11 -0400

    almost done

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate",3,"M","pink","good",True);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Rowdy",12,"M","purple","good",True);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Tabitha",5,"F","green","bad",False);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown",21,"M","orange","good",False);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Melissa",7,"F","brown","bad",False);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Grinch",9,"M","white","bad",True);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Wendy",1,"F","red","good",False);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES (NULL,NULL,"M","yellow",NULL,True);
