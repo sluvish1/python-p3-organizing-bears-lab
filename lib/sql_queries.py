6c33cde7978ef337715bbb5be64dd24968658bc5
Vaschell Mkenzie <mvaschell@gmail.com>
2023-08-03 16:54:55 -0400

    updated

--- lib/sql_queries.py
+++ lib/sql_queries.py
@@ -7,7 +7,7 @@ select_all_bears_names_and_orders_in_alphabetical_order = """
 """
 
 select_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest = """
-    SELECT name,age FROM bears WHERE alive = True ORDER BY age
+    SELECT name,age FROM bears WHERE alive = 1 ORDER BY age
 """
 
 select_oldest_bear_and_returns_name_and_age = """
